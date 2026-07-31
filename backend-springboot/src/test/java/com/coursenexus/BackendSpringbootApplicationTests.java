53111ed05640ff57bb9bf09baf26146518eeec49
Amit Bhagat <amitbhagat2506@gmail.com>
2026-07-31 11:37:38 +0530

    chore: initial project setup with Spring Boot configs

---
+++ backend-springboot/src/test/java/com/coursenexus/BackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursenexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
