da9e83160fe37f342e374dd7724e655e61fb1842
Amit Bhagat <amitbhagat2506@gmail.com>
2026-07-31 12:26:03 +0530

    feat: added Course entity schema for managing course details

---
+++ backend-springboot/src/main/java/com/coursenexus/entity/Course.java
@@ -0,0 +1,58 @@
+package com.coursenexus.entity;
+import java.util.List;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+
+    @Column(name = "course_id", updatable = false, nullable = false, columnDefinition = "BINARY(16)")
+    private UUID course_id;
+
+    @JsonProperty("course_name")
+    private String course_name;
+
+    private int price;
+
+    private String instructor;
+
+    private UUID instructorId;
+
+    private String description;
+
+    private String p_link;
+
+    private String y_link;
+
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Feedback> feedbacks;
+
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    private List<Video> videos;
+
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    private List<Learning> learnings;
+
+
+
+    @Column(nullable = false, columnDefinition = "boolean default false")
+    private boolean isApproved = false;
+}
+
