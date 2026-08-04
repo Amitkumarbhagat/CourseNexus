1ac55adf4bfdf2efd3cf3d9e2cdd3857cfefc17d
Amit Bhagat <amitbhagat2506@gmail.com>
2026-08-04 14:08:48 +0530

    Fix CORS issue

--- backend-springboot/src/main/java/com/coursenexus/config/WebSecurityConfig.java
+++ backend-springboot/src/main/java/com/coursenexus/config/WebSecurityConfig.java
@@ -80,7 +80,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://127.0.0.1:3000"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
